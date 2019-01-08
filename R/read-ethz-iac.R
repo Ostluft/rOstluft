eb57009c949356b29417eaaf441e328f60d66c74
Jörg Sintermann <joerg.sintermann@bd.zh.ch>
2019-01-08 20:15:31 +0100

    Zwischenstand Aufräumen

--- R/read-ethz-iac.R
+++ R/read-ethz-iac.R
@@ -1,17 +1,12 @@
-# gleich wie bei den meteo schweiz
 
-# bottom up ansatz
-
-# daten aus einer datei lesen
-
-read_ethz_10min_parameters <- function(x, site = NULL) {
+read_ethz_iac_min10_parameters <- function(x, site = NULL) {
   lines <- readr::read_lines(x, n_max = 60)
   from <- which(stringr::str_detect(lines, "9999"))[1]
   to <- which(lines == "5")
   header <- which(lines == "")[1]
   pars <-
     bind_cols(
-      parameter_original = as.character(readr::read_table2(con, skip = header - 1, n_max = 1, col_names = FALSE))[-1],
+      parameter_original = as.character(readr::read_table2(x, skip = header - 1, n_max = 1, col_names = FALSE))[-1],
       read_delim(x, delim = "(", skip = from, n_max = to - from - 1, col_names = FALSE)[,-1]
     ) %>%
     dplyr::rename(unit = X2)
@@ -23,8 +18,8 @@ read_ethz_10min_parameters <- function(x, site = NULL) {
 
 
 ### Funktion ETHZ IAC Daten einlesen
-read_ethz_iac_10min <- function(x, site = NULL, encoding = "UTF-8", timezone = "GMT") {
-  pars <- read_ethz_10min_parameters(x, site)
+read_ethz_iac_min10 <- function(x, site = NULL, encoding = "UTF-8", timezone = "Etc/GMT") {
+  pars <- read_ethz_iac_min10_parameters(x, site)
   if (is.null(site)) {site <- "ETHZ_CHN"}
   if (site == "-Hberg") {site <- "ETHZ_HBerg"}
   lines <- readr::read_lines(x, n_max = 60)
@@ -33,13 +28,13 @@ read_ethz_iac_10min <- function(x, site = NULL, encoding = "UTF-8", timezone = "
   startdate <- substring(startdate, 23, 32)
   df <-
     readr::read_table2(x, skip = header + 1, col_names = c("time", pars$parameter_original),
-                       na = c("99999.0", "9999.0"), skip_empty_rows = TRUE, locale = locale(encoding = encoding)) %>%
+                       na = c("99999.0", "9999.0", "-320"), skip_empty_rows = TRUE, locale = locale(encoding = encoding)) %>%
     mutate_all(as.numeric) %>%
     mutate(date = startdate) %>%
     gather(parameter_original, val, - date, -time) %>%
     mutate(
-      time = as.POSIXct(as.Date(date) + as.difftime(time, units = "mins"), tz = timezone),
-      interval = "10min",
+      starttime = as.POSIXct(as.Date(date) + as.difftime(time, units = "mins"), tz = timezone),
+      interval = "min10",
       site_short = site,
       unit = plyr::revalue(parameter_original, setNames(pars$unit, pars$parameter_original))
     ) %>%
@@ -47,16 +42,6 @@ read_ethz_iac_10min <- function(x, site = NULL, encoding = "UTF-8", timezone = "
   return(df)
 }
 
-
-
-
-# read_ethz_iac <- function(file, encoding = "latin1", tz = "Etc/GMT-1", time_shift = NULL) {
-#   # site information is in the file, could parse from the header or provide it as argument
-#   header <- readr::read_lines(file, n_max = 10L)
-#
-#   # invalid values include "99999.0", "9999.0", "-320" <- note sure how it is formatted
-#   data <- readr::read_table(file, skip = 10L) # needs some more parameter :P
-# }
 
 
 get_ethz_iac_con <- function(date, site, userpw = NULL, cache = NULL) {
--- R/read-smn.R
+++ R/read-smn.R
@@ -1,24 +1,10 @@
-# !!! work in progress
-
-# ! noch anpassen mit readr Funktionen, richtigem Zeichensatz encoding und tz
-# todo: Funktion zum MeteoSchweiz-Daten formatieren, optimieren & doku
-
-
-# aufbau sollte sein:
-# funktion die nur die konventierung von input nach format rolf übernimmt
-# automatischer download aktueller daten von ostluft als wrapper
-# fast copy & paste
-
-
-
 #' Reads a Swiss Meteo Network data file
 #'
 #' Reads the data from a Swiss Meteo Network export and returns it as a data frame in rOstluft long format
 #'
 #' @param file Either a path to a file, a connection, or literal data. see [readr::read_delim()]
-#' @param encoding file encoding. Default "latin1"
-#' @param tz time zone of date fields. Be carefull Etc/GMT + == -. Default "Etc/GMT-1"
-#' @param time_shift a lubridate period to add to the time. Default NULL
+#' @param encoding file encoding. Default "UTF-8"
+#' @param timezone time zone of date fields. Be carefull Etc/GMT + == -. Default "UTC"
 #'
 #' @return data frame in rOstluft long format
 #'
@@ -26,60 +12,7 @@
 #' @seealso [base::timezones] - Information about time zones in R
 #'
 #' @export
-read_smn_10min <- function(file, encoding = "latin1", tz = "Etc/GMT-1", time_shift = NULL) {
-  invisible(NULL)
-}
-
-
-read_smn_cap <- function(file, encoding = "latin1", tz = "Etc/GMT-1", time_shift = NULL) {
-  invisible(NULL)
-}
-
-
-#' Utility function to generate url and optional caching
-#'
-#' @param file
-#' @param cache if NULL no caching, if TRUE the data is cached in the user data dir, anything
-#'   else is used to create a path relative to current dir
-#' @param userpw
-#'
-#' @return
-#' @export
-get_smn_con <- function(file, cache = NULL, userpw = NULL) {
-  # problem for cache:
-  # we need always a base url to create a mapping from url to cache
-  # create a internal r6 class or function set for this
-  # then create hard coded wrappers for specific data locations
-
-  # can't lookup url from home
-  base_url <- "ftp://ftp.ostluft.ch/data_MeteoCH/Archiv/ftp_original/"
-
-  # no caching -> just return url readr automatically download http(s)/ftp(s) urls
-  if (is.null(cache)) {
-    return(RCurl::getURL(fs::path(base_url, file), userpw = userpw))
-  }
-
-  if (isTRUE(cache)) {
-    cache_path <- rappdirs::user_data_dir("cache", "rOstluft", "smn")
-  } else {
-    cache_path <- fs::path_abs(cache)
-  }
-
-  cache_path <- fs::path(cache_path, file)
-
-  if (!fs::file_exists(cache_path)) {
-    # download file with httr or rcurl to disk, message/warning/error by fail, argument as flag which case to use?
-    # resp <- httr::HEAD(chunk_url)
-    # if (httr::status_code(resp) == 200) {
-    #   httr::GET(chunk_url, httr::write_disk(chunk_path, overwrite = TRUE))
-    # }
-  }
-  return(cache_path)
-}
-
-
-
-read_MeteoCH <- function(x, timezone = "UTC", encoding = "UTF-8", time_format = "%Y%m%d%H%M") {
+read_meteoschweiz_smn <- function(x, timezone = "UTC", encoding = "UTF-8", time_format = "%Y%m%d%H%M") {
   lines <- readr::read_lines(x)
   lines <- lines[lines != ""]
   skip <- which(stringr::str_detect(lines, "time"))
@@ -105,12 +38,15 @@ read_MeteoCH <- function(x, timezone = "UTC", encoding = "UTF-8", time_format = 
       mutate_at(-id_cols, as.numeric) %>%
       gather(parameter_original, val, -id_cols) %>%
       mutate(
-        time = fast_strptime(time, format = time_format, lt = FALSE, tz = timezone),
+        starttime = fast_strptime(time, format = time_format, lt = FALSE, tz = timezone),
         unit = plyr::revalue(parameter_original, units)
-      )
+      ) %>%
+      dplyr::select(-time)
   }))
   return(df)
 }
+
+
 
 
 

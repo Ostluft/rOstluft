5f083e1bbda68ccf7c9ebb6634bc3d5ae945dec3
Thomas von Allmen <42243088+ugzvot@users.noreply.github.com>
2019-02-15 17:56:31 +0100

    prepare release: update doc, make devtools:check happy

--- R/read-smn.R
+++ R/read-smn.R
@@ -4,7 +4,9 @@
 #'
 #' @param x Either a path to a file, a connection, or literal data. see [readr::read_delim()]
 #' @param encoding file encoding. Default: "UTF-8"
-#' @param timezone time zone of date fields. Be carefull Etc/GMT '+' actually signifies '-' and vice versa, e.g. 'Etc/GMT-1' = UTC + 1. Default: "Etc/GMT"
+#' @param timezone time zone of date fields. Be carefull Etc/GMT '+' actually signifies '-' and vice versa, e.g.
+#'   'Etc/GMT-1' = UTC + 1. Default: "Etc/GMT"
+#' @param time_format of data in file, different for 10min and 1H
 #'
 #' @return tibble in rOstluft long format structure
 #'
@@ -26,22 +28,24 @@ read_meteoschweiz_smn <- function(x, timezone = "Etc/GMT", encoding = "UTF-8", t
     skip2 <- -1
     id_cols <- 1:2
   }
-  df <- bind_rows(lapply(1:length(skip), function(y) {
+  df <- dplyr::bind_rows(lapply(1:length(skip), function(y) {
     if (!is.null(units)) {
-      units <- readr::read_table2(x, skip = skip[y] - 1, col_names = TRUE, locale = locale(encoding = encoding), n_max = 1, skip_empty_rows = TRUE)
-      units <- setNames(as.character(units), names(units)[-c(1,2)])
-      units <- sapply(units[!is.na(units)], function(z) str_replace_all(z, "\\[|\\]", ""))
+      units <- readr::read_table2(x, skip = skip[y] - 1, col_names = TRUE, locale = readr::locale(encoding = encoding),
+                                  n_max = 1, skip_empty_rows = TRUE)
+      units <- rlang::set_names(as.character(units), names(units)[-c(1,2)])
+      units <- sapply(units[!is.na(units)], function(z) stringr::str_replace_all(z, "\\[|\\]", ""))
     }
-    readr::read_table2(x, skip = skip[y] - 1, col_names = TRUE, na = c("", "NA", "-"), locale = locale(encoding = encoding),
-                       n_max =  c(skip, Inf)[y + 1] - 2 - skip[y], skip_empty_rows = TRUE) %>%
+    readr::read_table2(x, skip = skip[y] - 1, col_names = TRUE, na = c("", "NA", "-"),
+                       locale = readr::locale(encoding = encoding), n_max =  c(skip, Inf)[y + 1] - 2 - skip[y],
+                       skip_empty_rows = TRUE) %>%
       dplyr::slice(skip2) %>%
-      mutate_at(-id_cols, as.numeric) %>%
-      gather(parameter_original, value, -id_cols) %>%
-      mutate(
-        starttime = fast_strptime(time, format = time_format, lt = FALSE, tz = timezone),
-        unit = plyr::revalue(parameter_original, units)
+      dplyr::mutate_at(-id_cols, as.numeric) %>%
+      tidyr::gather(.data$parameter_original, .data$value, -id_cols) %>%
+      dplyr::mutate(
+        starttime = lubridate::fast_strptime(.data$time, format = time_format, lt = FALSE, tz = timezone),
+        unit = plyr::revalue(.data$parameter_original, units)
       ) %>%
-      dplyr::select(-time)
+      dplyr::select(-.data$time)
   }))
   return(df)
 }

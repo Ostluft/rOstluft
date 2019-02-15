baeb8863fbc41cd7ce7c9d2e03ceafec5af94160
Jörg Sintermann <joerg.sintermann@bd.zh.ch>
2019-02-15 10:40:11 +0100

    function naming

--- R/transform-projection.R
+++ R/transform-projection.R
@@ -1,4 +1,4 @@
-transform.projection <- function(df, coord = c("lon", "lat"), initCRS = sp::CRS("+init=epsg:4326"),
+transform_projection <- function(df, coord = c("lon", "lat"), initCRS = sp::CRS("+init=epsg:4326"),
                                  outCRS = sp::CRS("+init=epsg:2056")) { #' CRS("+init=epsg:4326") = WGS84, CRS("+init=epsg:2056") = LV95
   sp::coordinates(df) <- coord
   sp::proj4string(df) <- initCRS

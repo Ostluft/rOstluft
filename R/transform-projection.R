94e1e4f11068fe78db9c6c62b8f8b10177b2d6d0
Jörg Sintermann <joerg.sintermann@bd.zh.ch>
2019-03-02 09:54:57 +0100

    added minimal roxygen

--- R/transform-projection.R
+++ R/transform-projection.R
@@ -1,3 +1,5 @@
+#' @title transform geographic projection
+#' @keywords internal
 transform_projection <- function(df, coord = c("lon", "lat"), initCRS = sp::CRS("+init=epsg:4326"),
                                  outCRS = sp::CRS("+init=epsg:2056")) { #' CRS("+init=epsg:4326") = WGS84, CRS("+init=epsg:2056") = LV95
   sp::coordinates(df) <- coord
@@ -5,3 +7,4 @@ transform_projection <- function(df, coord = c("lon", "lat"), initCRS = sp::CRS(
   df <- as.data.frame(sp::spTransform(df, outCRS))
   return(df)
 }
+

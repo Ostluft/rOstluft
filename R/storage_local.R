ed4046fa9c2d6cd9015304d72c34784ba269d5cb
Thomas von Allmen <42243088+ugzvot@users.noreply.github.com>
2019-07-09 13:21:59 +0200

    Unquoting language objects with `!!!` is deprecated as of rlang 0.4.0

--- R/storage_local.R
+++ R/storage_local.R
@@ -295,7 +295,7 @@ r6_storage_local <- R6::R6Class(
     read_chunk = function(chunk_path, filter) {
       chunk <- self$read_function(chunk_path)
       if (!rlang::quo_is_null(filter)) {
-        chunk <-  dplyr::filter(chunk, !!!filter)
+        chunk <-  dplyr::filter(chunk, !!filter)
       }
       chunk
     },
